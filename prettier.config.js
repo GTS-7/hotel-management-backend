1805b64c0eebe06dfbfbffb2b75d5180661e4de3
pavin <pavin227@gmail.com>
2025-03-15 11:21:53 +0530

    inital: initialization

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  quoteProps: "as-needed",
+  proseWrap: "preserve",
+};
+
+export default config;
