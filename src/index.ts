34ce7ff8d191194e11a6628d9d443e41c7e0e798
pavin <pavin227@gmail.com>
2025-03-29 12:21:52 +0530

    updates: changed the db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 
 // Importing the database connection
 import connectDB from './config/db.js';
@@ -12,14 +11,3 @@ const app = express();
 
 // Connection with MongoDB
 connectDB();
-
-// Checking the connection is connected or not with the database
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-
-    // Start server
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`);
-    });
-});
