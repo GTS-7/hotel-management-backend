326f796ea430f96ef65df7f88cdae8cb96d10c97
pavin <pavin227@gmail.com>
2025-04-03 23:07:48 +0530

    fixes: router spell mistake fixed

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 import user from "./user/index.js"
 const router = express.Router();
 
-router.post('/user', user);
+router.use('/user', user);
 
 export default router;
